7496a69078aec19bcb21571562745894749eef33
SummayaSiddiqui <summaya.siddiqui@keyin.com>
2024-04-07 02:59:48 -0230

    Option1

--- Taxi.py
+++ Taxi.py
@@ -5,7 +5,10 @@
 
 
 # Define required libraries.
-
+import datetime
+import FormatValues as FV
+import time
+import sys
 
 
 #constants# Define program constants.
@@ -19,7 +22,194 @@ WEEKLY_RENTAL_FEE       = float(f.readline()) #300.00
 HST_RATE                = float(f.readline()) #0.15
 
 def EnterNewEmployee():
-    pass
+    f = open('Defaults.dat', 'r')
+
+    NEXT_TRANSACTION_NUMBER = int(f.readline())   #143
+    NEXT_DRIVER_NUMBER      = int(f.readline())  #1922 
+    MONTHLY_STAND_FEE       = float(f.readline()) #175.00 
+    DAILY_RENTAL_FEE        = float(f.readline()) #60.00
+    WEEKLY_RENTAL_FEE       = float(f.readline()) #300.00
+    HST_RATE                = float(f.readline()) #0.15
+
+    f.close()
+    while True:
+    # Gather user inputs
+    
+    # Employee Name
+        while True:
+            FirstName = input("Enter employee first name: ").title()
+            if FirstName == "":
+                print("Data Entry Error - Employee first name cannot be blank.")
+            else:
+                break
+        while True:
+            LastName = input("Enter employee last name: ").title()
+            if LastName == "":
+                print("Data Entry Error - Employee first name cannot be blank.")
+            else:
+                break
+        EmployeeName = FirstName + " " + LastName
+
+        # Employee Address
+        StAddress = input("Enter employee street address: ").title()
+        City = input("Enter employee city: ").title()
+        Prov = input("Enter employee province (XX): ").upper()
+        
+        allowed_char = set("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+        allowed_num = set("0123456789")
+        while True:
+            PostalCode = input("Enter driver's postal code (X0X0X0): ").upper()
+            if PostalCode == "":
+                print("Error - Postal code cannot be blank.")
+            elif len(PostalCode) != 6:
+                print("Error - Postal code can be of 6 characrters only.")
+            elif set(PostalCode[0]).issubset(allowed_char) == False and set(PostalCode[2]).issubset(allowed_char) == False and set(PostalCode[4]).issubset(allowed_char) == False:
+                print("Error - The 1st, 3rd and 5th character in a postal code can be an alphabet only.")
+            elif set(PostalCode[1]).issubset(allowed_num) == False and set(PostalCode[3]).issubset(allowed_num) == False and set(PostalCode[5]).issubset(allowed_num) == False:
+                print("Error - The 1st, 3rd and 5th character in a postal code can be an alphabet only.")
+            else:
+                break
+        PostCode = PostalCode[0] + PostalCode[1] + PostalCode[2] + PostalCode[3] + PostalCode[4] + PostalCode[5]
+        
+        EmployeeAdd = StAddress + "," + " " + City + "," + " " + Prov + "," + " " + PostCode
+
+        # Employee Phone Number
+        while True:
+            PhoneNum = input("Enter employee phone number (9999999999): ")
+            if PhoneNum == "":
+                print("Data Entry Error - Phone number cannot be blank.")
+            elif PhoneNum.isdigit() == False:
+                print("Data Entry Error - Phone number contains digits only.")
+            elif len(PhoneNum) != 10:
+                print("Data Entry Error - Phone number contains 10 digits only.")
+            else:
+                break
+        EmployeePhone = "(" + PhoneNum[0:3] + ")" + " " + PhoneNum[3:6] + "-" + PhoneNum[6:11]
+
+        # Employee's License and Insurance Detail
+        LicenseNum = input("Enter driver's license number: ")
+        
+        while True:
+            try:
+                LicenseExpiryDate = input("Enter driver's license expiry date (YYYY-MM-DD): ")
+                LicenseExpiryDate = datetime.datetime.strptime(LicenseExpiryDate, "%Y-%m-%d")
+            except:
+                print("Data Entry Error - Expiry date is not in a valid format.")
+            else:
+                break
+        
+        InsuranceCompany = input("Enter insurance policy company: ").title()
+        PolicyNumber = input("Enter insurance policy number: ")
+
+        CarLst = ["O" , "R"]
+        while True:
+            CarType = input("Enter if the driver has his own car or a rented car (O / R): ").upper()
+            if CarType == "":
+                print("Data Entry Error - Car type can not be blank.")
+            elif CarType not in CarLst:
+                print("Data Entry Error - invalid entry.") 
+            elif CarType == "R":
+                NumDays = input("Enter the number of days car is rented for: ")
+                NumDays = int(NumDays)
+                break
+            else:
+                break
+    
+        BalDue = input("Enter the amount of balance due: ")
+        BalDue = float(BalDue)
+
+        # Perform required calculations.
+        if CarType == "O":
+            TotalRentalFee = 0
+            NumDays = 0
+            MonthlyFee = MONTHLY_STAND_FEE
+
+        if CarType == "R":
+            MonthlyFee = 0
+            if NumDays == 7:
+                TotalRentalFee = WEEKLY_RENTAL_FEE
+            elif NumDays >= 1 and NumDays < 7:
+                TotalRentalFee = DAILY_RENTAL_FEE * NumDays
+            
+
+        HST = (MonthlyFee + TotalRentalFee) * HST_RATE
+
+        TotalFee = MonthlyFee + TotalRentalFee + HST
+
+        # Display results
+        print()
+        print(f"            HAB TAXI SERVICES - EMPLOYEE DETAIL")
+        print(f"---------------------------------------------------------")
+        print()
+        print(f" Driver ID:                 {NEXT_DRIVER_NUMBER}")
+        print(f" Employee Name:             {EmployeeName}")
+        print(f" Employee Address:          {StAddress}")
+        print(f"                            {City + "," + " " + Prov + "," + " " + PostCode}")
+        print(f" Phone:                     {EmployeePhone}")
+        print()
+        print(f" License Number:            {LicenseNum:<10s}")
+        print(f" License Expiry Date:       {FV.FDateM(LicenseExpiryDate):<10s}")
+        print(f" Insurance Policy Number:   {PolicyNumber}")
+        print()
+        print(f"-----------------------------------------------------------")
+        print()
+        if CarType == "O":
+            TypeOfCar = "Own"
+        else:
+            TypeOfCar = "Rented"
+        print(f" Car Type: {TypeOfCar:<6s}           Monthly Stand Fee: {FV.FDollar2(MonthlyFee):>10s}")
+        print(f" Number of Rental Days: {NumDays}   Total Rental Fee:  {FV.FDollar2(TotalRentalFee):>10s}")
+        print(f"                            HST:               {FV.FDollar2(HST):>10s}")
+        print(f"----------------------------------------------------------")
+        print(f"                            Total Fee:         {FV.FDollar2(TotalFee):>10s}")
+        print(f"----------------------------------------------------------")
+        
+
+        # Write the employee details to a file called Employee.dat
+        for _ in range(5):  # Change to control no. of 'blinks'
+            print('Saving employee data ...', end='\r')
+            time.sleep(.3)  # To create the blinking effect
+            sys.stdout.write('\033[2K\r')  # Clears the entire line and carriage returns
+            time.sleep(.3)
+        
+        f = open('Employee.dat', 'a')
+
+        f.write("{}, ".format(str(NEXT_DRIVER_NUMBER)))
+        f.write("{}, ".format(str(EmployeeName)))
+        f.write("{}, ".format(EmployeeAdd))
+        f.write("{}, ".format(EmployeePhone))
+        f.write("{}, ".format(LicenseNum))
+        f.write("{}, ".format(FV.FDateM(LicenseExpiryDate)))
+        f.write("{}, ".format(str(InsuranceCompany)))
+        f.write("{}, ".format(str(PolicyNumber)))
+        f.write("{}, ".format(str(CarType)))
+        f.write("{}, ".format(str(MonthlyFee)))
+        f.write("{}\n".format(str(TotalRentalFee)))
+    
+        f.close()
+
+        print()
+        print("Employee data successfully saved ...", end='\r')
+        time.sleep(1)  # To create the blinking effect
+        sys.stdout.write('\033[2K\r')  # Clears the entire line and carriage returns
+
+        NEXT_DRIVER_NUMBER += 1
+        
+        Continue = input("Do you want to proceed with another policy (Y/N): ").capitalize()
+        if Continue == "Y":
+            print("You are all set to enter new driver's details.")
+            print()
+        else:
+            break
+        
+
+    f = open('Defaults.dat', 'w')
+    f.write("{}\n".format(str(NEXT_TRANSACTION_NUMBER)))
+    f.write("{}\n".format(str(NEXT_DRIVER_NUMBER)))
+    f.write("{}\n".format(str(MONTHLY_STAND_FEE)))
+    f.write("{}\n".format(str(DAILY_RENTAL_FEE)))
+    f.write("{}\n".format(str(WEEKLY_RENTAL_FEE)))
+    f.write("{}\n".format(str(HST_RATE)))
 
 def EnterCompanyRevenue():
     pass
